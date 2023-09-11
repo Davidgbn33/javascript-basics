b6ce19c5cf02199870e67d561c53a2e220e5743f
gibonDavid <gibon.david@live.fr>
2023-09-11 16:55:32 +0200

    test exo4

--- 12 - advanced-arrays/js/exercice4.js
+++ 12 - advanced-arrays/js/exercice4.js
@@ -1,7 +1,13 @@
-let morpion = [
-  (row1 = ["X", "O", "X"]),
-  (row2 = ["O", "X", "O"]),
-  (row3 = ["O", "X", "X"]),
-];
+let a = prompt("entrer un X ou O");
+let b = prompt("entrer un X ou O");
+let c = prompt("entrer un X ou O");
+let d = prompt("entrer un X ou O");
+let e = prompt("entrer un X ou O");
+let f = prompt("entrer un X ou O");
+let g = prompt("entrer un X ou O");
+let h = prompt("entrer un X ou O");
+let i = prompt("entrer un X ou O");
+
+let morpion = [(row1 = [a, b, c]), (row2 = [d, e, f]), (row3 = [g, h, i])];
 
 console.log(morpion);
