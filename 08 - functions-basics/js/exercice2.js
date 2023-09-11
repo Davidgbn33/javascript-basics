db7d090fd92446a196ad2bebeab350c7041708cc
gibonDavid <gibon.david@live.fr>
2023-09-11 14:12:20 +0200

    exo 1 to 3

--- 08 - functions-basics/js/exercice2.js
+++ 08 - functions-basics/js/exercice2.js
@@ -1,1 +1,14 @@
-// Your code goes here
+console.log("----------- \nExercie 2 \n ---------");
+
+let phrase = "ceci est un texte";
+
+function countWords(str) {
+  return str.split(" ").length;
+}
+
+function inversePhrase(str) {
+  return str.split(" ").reverse().join(" ");
+}
+
+console.log(countWords(phrase));
+console.log(inversePhrase(phrase));
