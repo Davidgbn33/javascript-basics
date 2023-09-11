6f8e02c72057c69bdf190ba4ed3df02596aca383
gibonDavid <gibon.david@live.fr>
2023-09-11 14:40:47 +0200

    add dir sous marin

---
+++ Sous-marins/script.js
@@ -0,0 +1,33 @@
+let myTitle = "les sous marins";
+
+let myOcean = "Nous nous trouvons dans la mer méditérannée";
+
+console.log(document.querySelector("#title"));
+
+document.querySelector("#title").innerHTML = myTitle;
+
+/* document.querySelector("#ocean").innerHTML = myOcean; */
+
+function generateNumber() {
+  let myNumber;
+  // Math.random() chiffre netre 0 et 1
+  myNumber = Math.floor(3 * Math.random());
+  // console.log(myNumber);
+  return myNumber;
+}
+
+myCell = generateNumber();
+console.log(myCell);
+
+tableRows = 5;
+tableColums = 5;
+let ocean = [];
+
+for (let i = 0; i < tableColums; i++) {
+  ocean[i] = [];
+  for (let j = 0; j < tableRows; j++) {
+    ocean[i][j] = generateNumber();
+  }
+}
+
+console.log(ocean);
