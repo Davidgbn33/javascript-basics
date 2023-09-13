545972d587a6fb7c430734a558aa71cb1f2aa89f
gibonDavid <gibon.david@live.fr>
2023-09-13 21:34:45 +0200

    js03 exo06

--- 03 - comparison/js/exercice5.js
+++ 03 - comparison/js/exercice5.js
@@ -1,1 +1,12 @@
-// Your code goes here
+let yearBirthday = prompt("Quel est ton année de naissance");
+
+let date = new Date();
+let currentYear = date.getFullYear();
+
+let age = currentYear - yearBirthday;
+
+if (age >= 18) {
+  alert(`Vous étes majeur et Vous avez ${age} ans`);
+} else {
+  alert(`Vous étes mineur et vous avez ${age} ans`);
+}
--- 03 - comparison/js/exercice6.js
+++ 03 - comparison/js/exercice6.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let table1 = [1, 5, 30];
+let table2 = [2, 7, 25];
+
+let elementCommun = false;
+
+for (let i = 0; i < table1.length; i++) {
+  for (let j = 0; j < table2.length; j++)
+    if (table1[i] === table2[j]) {
+      elementCommun = true;
+      break;
+    }
+  if (elementCommun) {
+    break;
+  }
+}
+if (elementCommun === true) {
+  alert("Les tableaux ont au moins un élément en commun");
+} else {
+  alert("Les tableaux ont aucun élémnet en commun");
+}
